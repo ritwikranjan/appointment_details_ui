71969cfda94f64c2564c13569cea1cc9b1bb7620
ritwikranjan <ritwikranjan99@gmail.com>
2020-07-31 19:47:08 +0530

    updated as asked

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:dotted_border/dotted_border.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,150 +17,186 @@ class MyApp extends StatelessWidget {
           leading: Icon(Icons.arrow_back),
         ),
         body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Row(
+            Card(
+              margin: EdgeInsets.all(20.0),
+              child: Column(
                 children: <Widget>[
-                  CircleAvatar(
-                    radius: 50.0,
-                    backgroundImage: NetworkImage(
-                        'https://pyxis.nymag.com/v1/imgs/846/9bb/440e83edacba3579e42bb6ad20860b50b0-18-arya-stark.rsquare.w700.jpg'),
-                  ),
                   Padding(
-                    padding: const EdgeInsets.only(left: 20.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                    padding: const EdgeInsets.all(20.0),
+                    child: Row(
                       children: <Widget>[
-                        Text(
-                          'Dr. Manoj Kumar',
-                          style: TextStyle(
-                              color: Colors.blueGrey[900],
-                              fontWeight: FontWeight.w600,
-                              fontSize: 25.0),
+                        CircleAvatar(
+                          radius: 50.0,
+                          backgroundImage: NetworkImage(
+                              'https://pyxis.nymag.com/v1/imgs/846/9bb/440e83edacba3579e42bb6ad20860b50b0-18-arya-stark.rsquare.w700.jpg'),
                         ),
                         Padding(
-                          padding: const EdgeInsets.only(top: 6.0),
-                          child: Text(
-                            'Cardiologist',
-                            style: TextStyle(
-                                color: Colors.blueGrey[900],
-                                fontWeight: FontWeight.w400,
-                                fontSize: 15.0),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 32.0),
-              child: Row(
-                children: <Widget>[
-                  Expanded(flex: 1, child: Icon(Icons.calendar_today)),
-                  Expanded(
-                    flex: 8,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: <Widget>[
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          padding: const EdgeInsets.only(left: 20.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
                             children: <Widget>[
-                              Text('Appointment Time'),
                               Text(
-                                'Time Left',
-                                style: TextStyle(fontSize: 10.0),
+                                'Dr. Manoj Kumar',
+                                style: TextStyle(
+                                    color: Colors.blueGrey[900],
+                                    fontWeight: FontWeight.w600,
+                                    fontSize: 25.0),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(top: 6.0),
+                                child: Text(
+                                  'Cardiologist',
+                                  style: TextStyle(
+                                      color: Colors.blueGrey[900],
+                                      fontWeight: FontWeight.w400,
+                                      fontSize: 15.0),
+                                ),
                               ),
                             ],
                           ),
                         ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 16.0, vertical: 4.0),
-                          child: Text('time'),
-                        ),
                       ],
                     ),
-                  )
-                ],
-              ),
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 32.0),
-              child: Row(
-                children: <Widget>[
-                  Expanded(flex: 1, child: Icon(Icons.location_on)),
-                  Expanded(
-                    flex: 8,
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text('Address'),
-                          Text('time'),
-                        ],
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 32.0),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Expanded(
-                    child: Icon(Icons.book),
-                    flex: 1,
                   ),
-                  Expanded(
-                    flex: 8,
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: <Widget>[
-                          Text('Booked For'),
-                          Container(
-                            padding: EdgeInsets.symmetric(horizontal: 4.0),
-                            margin: EdgeInsets.symmetric(vertical: 8.0),
-                            decoration: BoxDecoration(
-                              border: Border.all(width: 1.0),
-                              borderRadius: BorderRadius.circular(10.0),
-                            ),
-                            child: DropdownButton<String>(
-                              underline: Container(),
-                              items: <DropdownMenuItem<String>>[
-                                DropdownMenuItem(
-                                  child: Text('Ritwik'),
-                                )
-                              ],
-                              onChanged: (String value) {},
-                            ),
-                          )
-                        ],
-                      ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 32.0),
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(flex: 1, child: Icon(Icons.calendar_today)),
+                        Expanded(
+                          flex: 8,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: <Widget>[
+                              Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 16.0),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: <Widget>[
+                                    Text('Appointment Time'),
+                                  ],
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 16.0, vertical: 4.0),
+                                child: Text('time'),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
                     ),
-                  )
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 32.0),
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(flex: 1, child: Icon(Icons.location_on)),
+                        Expanded(
+                          flex: 8,
+                          child: Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 16.0),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text('Address'),
+                                Text('time'),
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 32.0),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Expanded(
+                          child: Icon(Icons.book),
+                          flex: 1,
+                        ),
+                        Expanded(
+                          flex: 8,
+                          child: Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 16.0),
+                            child: Padding(
+                              padding: const EdgeInsets.only(bottom: 16.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.stretch,
+                                children: <Widget>[
+                                  Text('Booked For'),
+                                  Text('Ritwik')
+                                ],
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ),
+            SizedBox(
+              height: 20.0,
+            ),
+            DottedBorder(
+              borderType: BorderType.RRect,
+              radius: Radius.circular(20.0),
+              child: Container(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text(
+                    'Support Number: 0965843',
+                    style: TextStyle(
+                      fontSize: 25.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.all(25.0),
+                  child: RaisedButton(
+                    onPressed: () {},
+                    color: Colors.blue,
+                    child: Text('Reschedule'),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(25.0),
+                  child: RaisedButton(
+                    padding: EdgeInsets.symmetric(horizontal: 30.0),
+                    onPressed: () {},
+                    color: Colors.green,
+                    child: Text('Cancel'),
+                  ),
+                )
+              ],
+            )
           ],
         ),
       ),
